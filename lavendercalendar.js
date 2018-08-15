410e8c3e3fff627257d56dc73c809dd026f9101d
Hailey <haileya4@nycischool.org>
2018-08-15 11:26:03 -0400

    initial commit

---
+++ lavendercalendar.js
@@ -0,0 +1,15 @@
+
+
+var m = new Date();
+var d = new Date();
+var w = new Date();
+var y = new Date();
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+month = document.getElementById("month").innerHTML = months[m.getMonth()];
+
+date = document.getElementById("date").innerHTML = d.getDate();
+
+weekdays = ["Sunday,","Monday,","Tuesday,","Wednesday,","Thursday,","Friday,","Saturday,"];
+weekday = document.getElementById("weekday").innerHTML = weekdays[w.getDay()];
+
+year = document.getElementById("year").innerHTML = y.getFullYear();
