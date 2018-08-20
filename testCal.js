068858886bf030e163f8c1954e72944fa5a4acbd
Shamadii <chamaojeda@gmail.com>
2018-08-20 13:44:17 -0400

    aesthetics

--- testCal.js
+++ testCal.js
@@ -322,20 +322,15 @@
 
 !function() {
   var data = [
-    { eventName: 'Lunch Meeting w/ Mark', calendar: 'Work', color: 'orange' },
-    { eventName: 'Interview - Jr. Web Developer', calendar: 'Work', color: 'orange' },
-    { eventName: 'Demo New App to the Board', calendar: 'Work', color: 'orange' },
-    { eventName: 'Dinner w/ Marketing', calendar: 'Work', color: 'orange' },
+    { eventName: 'Meet w/ Ms.Owen to review common app essay', calendar: 'College Stuff', color: 'orange' },
+    { eventName: 'Visit UPenn', calendar: "College Stuff", color: 'orange' },
+    { eventName: "Go to college fair", calendar: 'College Stuff', color: 'orange' },
+    { eventName: 'ask for recommendations', calendar: 'College Stuff', color: 'orange' },
 
     { eventName: 'Game vs Portalnd', calendar: 'Sports', color: 'blue' },
     { eventName: 'Game vs Houston', calendar: 'Sports', color: 'blue' },
     { eventName: 'Game vs Denver', calendar: 'Sports', color: 'blue' },
     { eventName: 'Game vs San Degio', calendar: 'Sports', color: 'blue' },
-
-    { eventName: 'School Play', calendar: 'Kids', color: 'yellow' },
-    { eventName: 'Parent/Teacher Conference', calendar: 'Kids', color: 'yellow' },
-    { eventName: 'Pick up from Soccer Practice', calendar: 'Kids', color: 'yellow' },
-    { eventName: 'Ice Cream Night', calendar: 'Kids', color: 'yellow' },
 
     { eventName: 'Free Tamale Night', calendar: 'Other', color: 'green' },
     { eventName: 'Bowling Team', calendar: 'Other', color: 'green' },
