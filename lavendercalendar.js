20f9883e15a9fff8e307d7edd82b97f17d91ef08
Maria <maria.molano0002@gmail.com>
2018-08-21 09:58:14 -0400

    log in stuff

--- lavendercalendar.js
+++ lavendercalendar.js
@@ -12,3 +12,13 @@
     weekday = document.getElementById("weekday").innerHTML = weekdays[w.getDay()];
 
     year = document.getElementById("year").innerHTML = y.getFullYear();
+
+    // Get the modal
+    var modal = document.getElementById('id01');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
